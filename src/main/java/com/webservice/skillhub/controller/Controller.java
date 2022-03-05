2b614d2b180e7e941da1513d0485107a73f8414c
Sheetal Jivannavar <sheetaljivannavar@gmail.com>
2022-03-05 15:53:22 +0530

    first commit

---
+++ src/main/java/com/webservice/skillhub/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.webservice.skillhub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class Controller {
+@GetMapping	
+public String getMethod() {
+	
+	return "Hello World!";
+	
+}
+}
---
+++ src/test/java/com/webservice/skillhub/SkillhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.skillhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
